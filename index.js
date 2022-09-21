b747695fbbb0c8f2d1fc5c84a2951c7fd73909ff
Sovattha Sok <github@soks.fr>
2022-09-21 23:49:41 +0200

    working version

--- index.js
+++ index.js
@@ -1,61 +1,93 @@
 const TradingView = require('@mathieuc/tradingview');
-var sub = require('date-fns/sub');
+const sub = require('date-fns/sub');
+const fs = require('fs');
 
 const client = new TradingView.Client();
 
-async function fetchPrices(symbol) {
-  const subs = [
-    { days: 0 },
-    { days: 7 },
-    { days: 30 },
-    { days: 90 },
-    { years: 1 },
-    { years: 2 },
-    { years: 3 },
-  ]//.map((sub) => getPrice(symbol, sub));
-  //return Promise.all(subs);
-  
-  const prices = [];
-  for (const sub of subs) {
-    const price = await getPrice(symbol, sub);
-    prices.push(price);
-  }
-  return prices;
+async function fetchPercentagesForCrypto(symbol) {
+  const queries = [
+    { symbol, subtracted: { days: 0 } },
+    { symbol, subtracted: { days: 7 } },
+    { symbol, subtracted: { days: 30 } },
+    { symbol, subtracted: { days: 90 } },
+    { symbol, subtracted: { years: 1 } },
+    { symbol, subtracted: { years: 2 } },
+    { symbol, subtracted: { years: 3 } },
+  ].map((query) => fetchPercentage(query));
+  return Promise.allSettled(queries);
+}
+
+function formatPctForCrypto(prices) {
+  const pcts = prices.filter((p) => p.status === 'fulfilled');
+  if (!pcts?.[0]?.value?.symbol) return;
+  return {
+    symbol: pcts[0].value.symbol,
+    description: pcts[0].value.description,
+    day_0: pcts.find((p) => p.value.subtracted.days === 0)?.value.pct || '',
+    day_7: pcts.find((p) => p.value.subtracted.days === 7)?.value.pct || '',
+    day_30: pcts.find((p) => p.value.subtracted.days === 30)?.value.pct || '',
+    day_90: pcts.find((p) => p.value.subtracted.days === 90)?.value.pct || '',
+    year_1: pcts.find((p) => p.value.subtracted.years === 1)?.value.pct || '',
+    year_2: pcts.find((p) => p.value.subtracted.years === 2)?.value.pct || '',
+    year_3: pcts.find((p) => p.value.subtracted.years === 3)?.value.pct || '',
+    url: pcts[0].value.url,
+  };
+}
+
+async function fetchPercentage(query) {
+  return new Promise((resolve, reject) => {
+    fetchChartData(query, resolve, reject);
+  });
+}
+
+function fetchChartData({ symbol, subtracted }, resolve, reject) {
+  const defaultTimeout = setTimeout(() => {
+    reject({
+      subtracted,
+      error: `Timeout for ${JSON.stringify(subtracted)}`,
+    });
+  }, 2000);
+  const chart = new client.Session.Chart();
+  chart.onError((...err) => {
+    // Listen for errors (can avoid crash)
+    console.error('Chart error:', ...err);
+  });
+  chart.onUpdate(() => {
+    clearTimeout(defaultTimeout);
+    if (!chart.periods[0])
+      return;
+    const response = {
+      time: new Date(chart.periods[0].time * 1000),
+      pct: chart.periods[0].close,
+    };
+    resolve({
+      symbol,
+      description: chart.infos.description,
+      subtracted,
+      ...response,
+      url: `https://www.tradingview.com/chart/?symbol=INTOTHEBLOCK:${symbol.toUpperCase()}_WHALESPERCENTAGE`
+    });
+  });
+  chart.setMarket(`INTOTHEBLOCK:${symbol.toUpperCase()}_WHALESPERCENTAGE`, {
+    timeframe: '1D',
+    range: 1,
+    to: sub(new Date(), subtracted).getTime() / 1000,
+  });
 }
 
 (async () => {
-    const prices = await fetchPrices('GHST');
-console.log(prices)
+  try {
+    const cryptos = fs.readFileSync('./cryptos.txt', 'utf-8').split(/\r?\n/).filter(Boolean);
+    console.log(`✨ Fetching data for ${cryptos.length} cryptos...`);
+    const pcts = await Promise.all(
+      cryptos.map((crypto) =>
+        fetchPercentagesForCrypto(crypto).then(formatPctForCrypto)
+      )
+    );
+    fs.writeFileSync('./whales.json', JSON.stringify(pcts, null, 2));
+    console.log(`🚀 File whales.json generated for ${cryptos.length} cryptos!`);
+    client.end();
+  } catch (error) {
+    console.error('Error fetching percentages for crypto:', error);
+  }
 })();
-
-  const timer = setTimeout(() => {fetchPrices('GHST')}, 1000000)
-  
-
-async function getPrice(symbol, subtracted) {
-  console.log(symbol, subtracted);
-  return new Promise((resolve, reject) => {
-    try {
-      const chart = new client.Session.Chart();
-      chart.setMarket(`INTOTHEBLOCK:${symbol.toUpperCase()}_WHALESPERCENTAGE`, {
-        timeframe: '1D',
-        range: 1, // Can be positive to get before or negative to get after
-        to: sub(new Date(), subtracted).getTime() / 1000,
-      });
-      chart.onUpdate(() => {
-        const response = chart.periods.map((p) => ({
-          symbol: 'GHST',
-          time: new Date(p.time * 1000),
-          close: p.close,
-        }));
-        console.log('======', subtracted, response);
-        resolve(
-          response
-        );
-        client.end();
-      });
-    } catch (error) {
-      console.error(error);
-      reject(error);
-    }
-  });
-}
